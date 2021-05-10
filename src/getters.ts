fc9d8e3840eb7a6a9bafaaab3c79764419bd8735
Oleg Valter <o.a.philippov@gmail.com>
2021-05-10 05:12:51 +0300

    Removed options preventing build

--- src/getters.ts
+++ src/getters.ts
@@ -1,5 +1,5 @@
 import { config } from "./config";
-import { safeMatch } from "./utils";
+import { handleMatchFailure, safeMatch } from "./utils";
 
 export const getAnswerId = (selector: string) => {
   const link = document.querySelector<HTMLAnchorElement>(selector);
@@ -16,3 +16,27 @@ export const getPostId = ({
     page: { links },
   },
 }: typeof config) => getAnswerId(links.answer) || getQuestionId(links.question);
+
+export const getEditAuthorId = () => {
+  const postWrapSelector = config.selectors.info.post.wrapper;
+
+  const spans = document.querySelectorAll(postWrapSelector);
+  if (!spans.length) return handleMatchFailure(postWrapSelector, null);
+
+  const userSpan = Array.from(spans).find(({ textContent }) =>
+    /proposed/i.test(textContent || "")
+  );
+  if (!userSpan) return null;
+
+  const cardSelector = config.selectors.info.editor.card;
+
+  const { parentElement } = userSpan;
+  const link = parentElement!.querySelector<HTMLAnchorElement>(cardSelector);
+  if (!link) return handleMatchFailure(cardSelector, null);
+
+  const { href } = link;
+  const [, userId] = href.match(/users\/(\d+)/) || [];
+  if (!userId) return null;
+
+  return userId;
+};
