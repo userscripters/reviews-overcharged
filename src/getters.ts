9fd3db523f7fd1d7b298959fde85ad5797632289
Oleg Valter <o.a.philippov@gmail.com>
2021-05-10 05:07:55 +0300

    Moved general getters to module

---
+++ src/getters.ts
@@ -0,0 +1,18 @@
+import { config } from "./main";
+import { safeMatch } from "./utils";
+
+export const getAnswerId = (selector: string) => {
+  const link = document.querySelector<HTMLAnchorElement>(selector);
+  return safeMatch(link?.href || "", /\/questions\/\d+\/[\w-]+\/(\d+)/, "")[0];
+};
+
+export const getQuestionId = (selector: string) => {
+  const link = document.querySelector<HTMLAnchorElement>(selector);
+  return safeMatch(link?.href || "", /\/questions\/(\d+)/, "")[0];
+};
+
+export const getPostId = ({
+  selectors: {
+    page: { links },
+  },
+}: typeof config) => getAnswerId(links.answer) || getQuestionId(links.question);
