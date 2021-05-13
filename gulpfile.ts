568390af5963268ba21ecc139e3d5b762c4f515e
Oleg Valter <o.a.philippov@gmail.com>
2021-05-14 00:37:47 +0300

    finished Gulp build flow

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,20 +1,43 @@
-import browserify from "browserify";
-import type GulpClient from "gulp";
+import * as browserify from "browserify";
+import * as del from "del";
+import * as GulpClient from "gulp";
 import { dest, src } from "gulp";
-import ts from "gulp-typescript";
-import ug from "gulp-uglify";
+import * as ts from "gulp-typescript";
+import * as ug from "gulp-uglify";
+import * as buffer from "vinyl-buffer";
+import * as source from "vinyl-source-stream";
 
-const project = ts.createProject("./tsconfig.json");
+const project = ts.createProject("./src/tsconfig.json");
 
-export const build: GulpClient.TaskFunction = () => {
-  return src("src/*.ts")
-    .pipe(project())
-    .pipe(ug({ output: { webkit: true } }))
-    .pipe(dest("dist"))
-    .on("end", () => {
-      const bobj = browserify({
-        entries: "dist/main.js",
-      });
-      return bobj.bundle();
-    });
+export const build: GulpClient.TaskFunction = async () => {
+  const transforming = new Promise((resolve, reject) => {
+    return src("src/*.ts")
+      .pipe(project())
+      .pipe(dest("dist"))
+      .on("error", reject)
+      .on("end", resolve);
+  });
+
+  await transforming;
+
+  const entryPath = "dist/main.js";
+
+  const bobj = browserify({
+    entries: entryPath,
+  });
+
+  const bundling = new Promise((resolve, reject) => {
+    bobj
+      .bundle()
+      .pipe(source(entryPath))
+      .pipe(buffer())
+      .pipe(ug({ output: { webkit: true } }))
+      .pipe(dest(".", { overwrite: true }))
+      .on("error", reject)
+      .on("end", resolve);
+  });
+
+  await bundling;
+
+  del(["dist/**/*", "!dist/main.js"]);
 };
