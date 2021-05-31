67a4061dda4a1cad41692984f05f4eacc0ff0af9
Oleg Valter <o.a.philippov@gmail.com>
2021-05-31 12:20:41 +0300

    Movedd stats creators to module

--- src/getters.ts
+++ src/getters.ts
@@ -1,79 +1,165 @@
 import { config } from "./config";
+import { arraySelect } from "./domUtils";
 import { handleMatchFailure, safeMatch } from "./utils";
 
 export type SuggestedEditInfo = {
-  approval_date?: number;
-  comment: string;
-  creation_date: number;
-  post_id: number;
-  post_type: "question" | "answer" | "article";
-  proposing_user?: {}; //TODO: expand
-  rejection_date?: string;
-  suggested_edit_id: number;
-  tags: string[];
-  title: string;
+    approval_date?: number;
+    comment: string;
+    creation_date: number;
+    post_id: number;
+    post_type: "question" | "answer" | "article";
+    proposing_user?: {}; //TODO: expand
+    rejection_date?: string;
+    suggested_edit_id: number;
+    tags: string[];
+    title: string;
 };
 
 export const getAnswerId = (selector: string) => {
-  const link = document.querySelector<HTMLAnchorElement>(selector);
-  return safeMatch(link?.href || "", /\/questions\/\d+\/[\w-]+\/(\d+)/, "")[0];
+    const link = document.querySelector<HTMLAnchorElement>(selector);
+    return safeMatch(
+        link?.href || "",
+        /\/questions\/\d+\/[\w-]+\/(\d+)/,
+        ""
+    )[0];
 };
 
 export const getQuestionId = (selector: string) => {
-  const link = document.querySelector<HTMLAnchorElement>(selector);
-  return safeMatch(link?.href || "", /\/questions\/(\d+)/, "")[0];
+    const link = document.querySelector<HTMLAnchorElement>(selector);
+    return safeMatch(link?.href || "", /\/questions\/(\d+)/, "")[0];
 };
 
 export const getPostId = ({
-  selectors: {
-    page: { links },
-  },
+    selectors: {
+        page: { links },
+    },
 }: typeof config) => getAnswerId(links.answer) || getQuestionId(links.question);
 
 export const getEditAuthorId = () => {
-  const postWrapSelector = config.selectors.info.post.wrapper;
+    const postWrapSelector = config.selectors.info.post.wrapper;
 
-  const spans = document.querySelectorAll(postWrapSelector);
-  if (!spans.length) return handleMatchFailure(postWrapSelector, null);
+    const spans = document.querySelectorAll(postWrapSelector);
+    if (!spans.length) return handleMatchFailure(postWrapSelector, null);
 
-  const userSpan = Array.from(spans).find(({ textContent }) =>
-    /proposed/i.test(textContent || "")
-  );
-  if (!userSpan) return null;
+    const userSpan = Array.from(spans).find(({ textContent }) =>
+        /proposed/i.test(textContent || "")
+    );
+    if (!userSpan) return null;
 
-  const cardSelector = config.selectors.info.editor.card;
+    const cardSelector = config.selectors.info.editor.card;
 
-  const { parentElement } = userSpan;
-  const link = parentElement!.querySelector<HTMLAnchorElement>(cardSelector);
-  if (!link) return handleMatchFailure(cardSelector, null);
+    const { parentElement } = userSpan;
+    const link = parentElement!.querySelector<HTMLAnchorElement>(cardSelector);
+    if (!link) return handleMatchFailure(cardSelector, null);
 
-  const { href } = link;
-  const [, userId] = href.match(/users\/(\d+)/) || [];
-  if (!userId) return null;
+    const { href } = link;
+    const [, userId] = href.match(/users\/(\d+)/) || [];
+    if (!userId) return null;
 
-  return userId;
+    return userId;
 };
 
 export const getSuggestionTotals = (suggestions: SuggestedEditInfo[]) => {
-  const stats = {
-    get ratio() {
-      const { approved, rejected, total } = this;
-      return {
-        ofApproved: approved / total,
-        ofRejected: rejected / total,
-        approvedToRejected: approved / (rejected === 0 ? 1 : rejected),
-      };
-    },
-    approved: 0,
-    rejected: 0,
-    total: 0,
-  };
+    const stats = {
+        get ratio() {
+            const { approved, rejected, total } = this;
+            return {
+                ofApproved: approved / total,
+                ofRejected: rejected / total,
+                approvedToRejected: approved / (rejected === 0 ? 1 : rejected),
+            };
+        },
+        approved: 0,
+        rejected: 0,
+        total: 0,
+    };
 
-  suggestions.forEach(({ approval_date, rejection_date }) => {
-    stats.total += 1;
-    if (approval_date) stats.approved += 1;
-    if (rejection_date) stats.rejected += 1;
-  });
+    suggestions.forEach(({ approval_date, rejection_date }) => {
+        stats.total += 1;
+        if (approval_date) stats.approved += 1;
+        if (rejection_date) stats.rejected += 1;
+    });
 
-  return stats;
+    return stats;
+};
+
+export type RejectionCount = {
+    spam: number;
+    improvement: number;
+    intent: number;
+    reply: number;
+    harm: number;
+};
+
+const callRejectionModal = (cnf: typeof config) => {
+    const {
+        selectors: {
+            buttons,
+            actions: { inputs, modal },
+        },
+    } = cnf;
+
+    const rejectInput = document.querySelector<HTMLInputElement>(inputs.reject);
+    const submitButton = document.querySelector<HTMLButtonElement>(
+        buttons.submit
+    );
+    if (!rejectInput || !submitButton) return null;
+
+    rejectInput.click();
+    submitButton.click();
+
+    const modalWrapper = document.querySelector<HTMLFormElement>(modal.form);
+    if (!modalWrapper) return null;
+
+    const dolly = modalWrapper.cloneNode(true) as HTMLDivElement;
+
+    const closeBtn = modalWrapper.querySelector<HTMLButtonElement>(
+        buttons.close
+    )!;
+
+    closeBtn.click();
+    return dolly;
+};
+
+export const getRejectionCount = (cnf: typeof config) => {
+    const {
+        selectors: {
+            actions: { modal },
+        },
+    } = cnf;
+
+    const modalWrapper = callRejectionModal(cnf);
+    if (!modalWrapper) return handleMatchFailure(modal.form, null);
+
+    const withVotes = arraySelect<HTMLLabelElement>(
+        modalWrapper,
+        modal.votes.labels
+    );
+
+    const count: RejectionCount = {
+        spam: 0,
+        improvement: 0,
+        intent: 0,
+        reply: 0,
+        harm: 0,
+    };
+
+    const reasonMap: { [P in keyof RejectionCount as string]: P } = {
+        102: "improvement",
+        101: "spam",
+        104: "intent",
+        105: "reply",
+        0: "harm",
+    };
+
+    const voteSelector = modal.votes.counts;
+
+    withVotes.forEach((label) => {
+        const { htmlFor } = label;
+        const [_full, reasonId] = htmlFor.match(/(\d+$)/) || [];
+        const reason = reasonMap[reasonId];
+        if (label.querySelector(voteSelector)) count[reason] += 1;
+    });
+
+    return count;
 };
---
+++ src/stats.ts
@@ -0,0 +1,122 @@
+import { getSuggestionsUserStats } from "./api";
+import { config } from "./config";
+import { createItem } from "./dom";
+import {
+    getEditAuthorId,
+    getRejectionCount,
+    getSuggestionTotals,
+    RejectionCount,
+    SuggestedEditInfo,
+} from "./getters";
+import { a, br, ListOptions, p, text, ul } from "./templaters";
+import { getUserInfo, UserInfo } from "./users";
+import { scase, toPercent } from "./utils";
+
+export const createEditAuthorItem = ({
+    display_name,
+    reputation,
+    link,
+}: UserInfo) => {
+    const namePar = p(`Name: `);
+    namePar.append(a(link, display_name));
+
+    return createItem(
+        ul({
+            header: "Edit Author",
+            items: [namePar, `Reputation: ${reputation}`],
+        })
+    );
+};
+
+export const createEditorStatsItem = (
+    { link }: UserInfo,
+    suggestions: SuggestedEditInfo[]
+) => {
+    const {
+        approved,
+        rejected,
+        total,
+        ratio: { approvedToRejected, ofApproved, ofRejected },
+    } = getSuggestionTotals(suggestions);
+
+    const itemParams: ListOptions = {
+        header: "Author Stats",
+        items: [],
+    };
+
+    if (!total) {
+        const infoPar = p(`Tag wiki/excerpt edits are not returned.`);
+
+        infoPar.append(
+            br(),
+            text(`See their `),
+            a(`${link}?tab=activity`, "activity tab")
+        );
+
+        itemParams.items.push(infoPar);
+        return createItem(ul(itemParams));
+    }
+
+    itemParams.items.push(
+        `Approved: ${approved} (${toPercent(ofApproved)})`,
+        `Rejected: ${rejected} (${toPercent(ofRejected)})`,
+        `Of total: ${total}`,
+        `Ratio: ${approvedToRejected}`
+    );
+
+    return createItem(ul(itemParams));
+};
+
+export const createRejectionCountItem = (count: RejectionCount) => {
+    const withVotes = Object.entries(count).filter(([_k, v]) => !!v);
+    const items = withVotes.map(([k, v]) => `${scase(k)}: ${v}`);
+
+    if (!items.length) items.push("No reject votes");
+
+    return createItem(ul({ items, header: "Reject votes" }));
+};
+
+export const addStatsSidebar = async (cnf: typeof config) => {
+    const sidebar = document.querySelector(cnf.selectors.actions.sidebar);
+
+    if (!sidebar) return false;
+
+    const dialog = document.createElement("div");
+    dialog.classList.add("s-sidebarwidget", "ml24", "mt24");
+
+    const header = document.createElement("div");
+    header.classList.add("s-sidebarwidget--header");
+    header.textContent = "Extra Info";
+
+    const itemWrap = document.createElement("div");
+    itemWrap.classList.add(cnf.classes.grid.container, "fd-column");
+
+    const authorId = getEditAuthorId();
+
+    if (!authorId) return false;
+
+    const [editAuthorInfo, editAuthorStats] = await Promise.all([
+        getUserInfo(authorId),
+        getSuggestionsUserStats(authorId),
+    ]);
+
+    const rejectCount = getRejectionCount(cnf);
+
+    if (!editAuthorInfo || !rejectCount) return false;
+
+    const items: HTMLDivElement[] = [];
+
+    items.push(
+        createEditAuthorItem(editAuthorInfo),
+        createEditorStatsItem(editAuthorInfo, editAuthorStats),
+        createRejectionCountItem(rejectCount)
+    );
+
+    itemWrap.append(...items);
+
+    dialog.append(header, itemWrap);
+
+    editAuthorInfo && sidebar.append(dialog);
+
+    return true;
+};
