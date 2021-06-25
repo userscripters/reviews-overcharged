4153dcd24b2dea6c360c62643f77fb2a4c8ecfa7
Oleg Valter <o.a.philippov@gmail.com>
2021-06-26 02:06:05 +0300

    added headers autogeneration

---
+++ dist/headers.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author          Oleg Valter
+// @description    
+// @homepage        https://github.com/userscripters/reviews-overcharged#readme
+// @match           ://*.askubuntu.com/review/suggested-edits/*
+// @match           ://*.mathoverflow.net/review/suggested-edits/*
+// @match           ://*.serverfault.com/review/suggested-edits/*
+// @match           ://*.stackapps.com/review/suggested-edits/*
+// @match           ://*.stackexchange.com/review/suggested-edits/*
+// @match           ://*.stackoverflow.com/review/suggested-edits/*
+// @match           ://*.superuser.com/review/suggested-edits/*
+// @name            reviews-overcharged
+// @source          git+https://github.com/userscripters/reviews-overcharged.git
+// @supportURL      https://github.com/userscripters/reviews-overcharged/issues
+// @version         1.0.0
+// ==/UserScript==
