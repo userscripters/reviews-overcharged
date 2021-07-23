352bac54d0c5c7ada2f6b0db0f8a76565ac32139
Oleg Valter <o.a.philippov@gmail.com>
2021-07-23 04:51:13 +0300

    added util for getting review id

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 export const handleMatchFailure = <R extends null | false>(
-  selector: string,
-  returnValue: R
+    selector: string,
+    returnValue: R
 ) => {
-  console.debug(`Couldn't find the element with selector: ${selector}`);
-  return returnValue;
+    console.debug(`Couldn't find the element with selector: ${selector}`);
+    return returnValue;
 };
 
 export const last = <A extends any[]>(arr: A): A[number] => arr[arr.length - 1];
@@ -11,13 +11,15 @@ export const last = <A extends any[]>(arr: A): A[number] => arr[arr.length - 1];
 type SafeMatchReturn = [full: string, group1: string, ...others: string[]];
 
 export const safeMatch = (text: string, regex: RegExp, def = "") =>
-  (text.match(regex) || [text, def]).slice(1) as SafeMatchReturn;
+    (text.match(regex) || [text, def]).slice(1) as SafeMatchReturn;
 
 export const scase = (word: string) =>
-  word[0].toUpperCase() + word.slice(1).toLowerCase();
+    word[0].toUpperCase() + word.slice(1).toLowerCase();
 
 export const toApiDate = (date: Date) => (date.valueOf() / 1e3).toString();
 
 export const toPercent = (ratio: number) => `${Math.trunc(ratio * 100)}%`;
 
 export const trimNumericString = (text: string) => text.replace(/\D/g, "");
+
+export const getReviewId = () => last(location.href.split("/"));
