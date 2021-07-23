dc0fd08967e0ff2407f33685c18f41031f8c133f
Oleg Valter <o.a.philippov@gmail.com>
2021-07-23 04:52:13 +0300

    improved header generator shell script

--- scripts/headers.sh
+++ scripts/headers.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-generate tampermonkey -o dist/headers.js -m $(cat .matches)
+dist="dist"
+output=$dist"/headers.js"
+
+generate tampermonkey \
+    -o $output \
+    -m $(cat .matches)
+
+userscript=$dist"/$(ls $dist -1 | grep -e "main\.js")"
+
+sed -i -e "{1e cat $output; echo; echo" -e "; N}" "$userscript"
