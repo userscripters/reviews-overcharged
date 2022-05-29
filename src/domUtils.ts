67f9550d23e0be64d8e597b5ca93de0e355b4322
Oleg Valter <o.a.philippov@gmail.com>
2022-05-29 09:12:40 +0300

    added timeout to waitForSelector util

--- src/domUtils.ts
+++ src/domUtils.ts
@@ -11,17 +11,26 @@ export const goParentUp = (
     return goParentUp(element.parentElement, times - 1);
 };
 
+/**
+ * @summary waits until elements matching {@link selector} appear in DOM
+ * @param selector selector to match
+ * @param timeout optional timeout
+ */
 export const waitForSelector = <T extends Element>(
-    selector: string
+    selector: string,
+    timeout?: number
 ): Promise<NodeListOf<T>> => {
     const initial = document.querySelectorAll<T>(selector);
     if (initial.length) return Promise.resolve(initial);
 
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+        const handle = timeout && setTimeout(reject, timeout);
+
         const observer = new MutationObserver(() => {
             const target = document.querySelectorAll<T>(selector);
             if (!target.length) return;
 
+            clearTimeout(handle);
             observer.disconnect();
             resolve(target);
         });
