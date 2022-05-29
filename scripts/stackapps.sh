6eba24ce645c0830bdcaea5308372b0bf7222951
Oleg Valter <o.a.philippov@gmail.com>
2022-05-29 12:38:02 +0300

    minor fixes for build scripts

--- scripts/stackapps.sh
+++ scripts/stackapps.sh
@@ -1,7 +1,6 @@
 generate-stackapps \
     --chrome "101.0.4951.67" \
     --install "https://github.com/userscripters/reviews-overcharged/raw/master/dist/modern/index.user.js" \
-    --minified "https://github.com/userscripters/reviews-overcharged/raw/master/dist/modern/index.min.user.js" \
     --language "TypeScript" \
     --org-name "UserScripters" \
     --org-url "https://github.com/userscripters" \
